bbf13475d00c1f1e00f2a16b12d4d00d29fe7242
fukicycle <lego.sato.4135@gmail.com>
2025-09-05 11:58:06 +0900

    update pwa style

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/english-master-react/'
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        name: "English Master",
+        short_name: "EM",
+        description: "English Master with React",
+        start_url: ".",
+        display: "standalone",
+        orientation: "portrait",
+        theme_color: "#0ea4e9",
+        background_color: "#0ea4e9",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/english-master-react/",
+});
